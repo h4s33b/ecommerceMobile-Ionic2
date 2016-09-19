04854a02a71d6602a7b2a0f800ef32351ee666fb
h4s33b <haseeb.rehman@iunc.edu.pk>
2016-09-19 23:39:01 +0500

    initialized cart service.

---
+++ app/services/cart/cart.service.ts
@@ -0,0 +1,36 @@
+import {product} from '../../interfaces/product';
+
+export class cartService{
+
+    constructor() {
+
+    }
+
+    getAllProducts(){
+
+    }
+
+    getSingleProduct(id:number){
+
+    }
+
+    addProduct(prod:product,quantity:number){
+
+    }
+
+    updateProduct(prod:product,quantity:number){
+
+    }
+
+    removeProduct(id:number){
+
+    }
+
+    getTotalCartAmount(){
+
+    }
+
+    clearCart(){
+        
+    }
+}
